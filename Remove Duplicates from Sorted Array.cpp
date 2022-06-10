dfa5b35409c309bf18e638030427dc08a5dd67ab
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2022-06-10 17:47:10 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(m[nums[i]]==0)
-            {
-                m[nums[i]]=1;
-            }
-            else
-            {
-                m[nums[i]]++;
-            }
-        }
-        int k=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(m[nums[i]]==1)
-            {
-                nums[k]=nums[i];
-                k++;
+        if(nums.size() == 0) return 0;
+        int left = 0;
+        for(int right =1; right< nums.size(); right++){
+           if(nums[left] != nums[right])
+           {
+                left++;
+           }
+              
+               nums[left] = nums[right];
+           }
+    return left+1;
+        //using o(n) space
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(m[nums[i]]==0)
+//             {
+//                 m[nums[i]]=1;
+//             }
+//             else
+//             {
+//                 m[nums[i]]++;
+//             }
+//         }
+//         int k=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(m[nums[i]]==1)
+//             {
+//                 nums[k]=nums[i];
+//                 k++;
                 
-            }
-            else if(m[nums[i]]>1)
-            {
-                nums[k]=nums[i];
-                k++;
-                m[nums[i]]=0;
-            }
-        }
-        return k;
+//             }
+//             else if(m[nums[i]]>1)
+//             {
+//                 nums[k]=nums[i];
+//                 k++;
+//                 m[nums[i]]=0;
+//             }
+//         }
+//         return k;
     }
 };
