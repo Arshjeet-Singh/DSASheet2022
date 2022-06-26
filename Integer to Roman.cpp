ad1a4b4feedbc53c79fc39b900b76ba0215458c6
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2022-06-26 08:52:11 +0530

    Create Integer to Roman.cpp

---
+++ Integer to Roman.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s1="";
+            while(num>=1000)
+            {
+              s1+="M";
+                num-=1000;
+            }
+             while(num>=900)
+            {
+              s1+="CM";
+               num-=900;
+            }
+         while(num>=500)
+            {
+              s1+="D";
+                num-=500;
+            }
+         while(num>=400)
+            {
+              s1+="CD";
+                num-=400;
+            }
+         while(num>=100)
+            {
+              s1+="C";
+                num-=100;
+            }
+         while(num>=90)
+            {
+              s1+="XC";
+                num-=90;
+            }
+         while(num>=50)
+            {
+              s1+="L";
+                num-=50;
+            }
+         while(num>=40)
+            {
+              s1+="XL";
+                num-=40;
+            }
+         while(num>=10)
+            {
+              s1+="X";
+                num-=10;
+            }
+         while(num>=9)
+            {
+              s1+="IX";
+                num-=9;
+            }
+         while(num>=5)
+            {
+              s1+="V";
+                num-=5;
+            }
+         while(num>=4)
+            {
+              s1+="IV";
+                num-=4;
+            }
+         while(num>=1)
+            {
+              s1+="I";
+                num-=1;
+            }
+        return s1;
+    }
+};
