3f47a69c98f6f0986e2abac3e0a081fdfcfb6738
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2022-07-18 12:45:14 +0530

    Create Reverse Integer.cpp

---
+++ Reverse Integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int reverse(int x) {
+        // int flag=0;
+        // if(x<0)
+        // {
+        //     flag=1;
+        // }
+        int rev=0;
+        while(x!=0)
+        {
+            int r=x%10;
+           
+            if(rev>(INT_MAX)/10 || rev<(INT_MIN)/10)
+            {
+                return 0;
+            }
+             rev=rev*10+r;
+            x/=10;
+        }
+        // if(flag==1)
+        // {
+        //     return -1*rev;
+        // }
+        // else
+        // {
+            return rev;
+        // }
+    }
+};
