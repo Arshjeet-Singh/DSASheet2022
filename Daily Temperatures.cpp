c5f60412940da0b4b3122c7d334e04361fb207ab
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2022-08-19 01:04:23 -0700

    Create Daily Temperatures.cpp

---
+++ Daily Temperatures.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        vector<int> ans(n,0);
+        int maxi=INT_MIN;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(temperatures[i]>maxi)
+            {
+                maxi=temperatures[i];
+            }
+            else
+            {
+                int it=i+1;
+                while(temperatures[it]<=temperatures[i])
+                {
+                    it=it+ans[it];
+                }
+                ans[i]=it-i;
+            }
+        }
+        return ans;
+    }
+};
