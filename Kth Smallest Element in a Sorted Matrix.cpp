6f96147ed22c2fa493900702495f04acb630a9c5
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2022-08-27 10:50:45 +0530

    Create Kth Smallest Element in a Sorted Matrix.cpp

---
+++ Kth Smallest Element in a Sorted Matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            int count=0;
+            for(int i=0;i<n;i++)
+            {
+                count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+            }
+            if(count<k)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }
+        return low;
+        //with extra space
+        // priority_queue<int> q;
+        // int n=matrix.size();
+        // int m=matrix[0].size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         q.push(matrix[i][j]);
+        //         if(q.size()>k)
+        //         {
+        //             q.pop();
+        //         }
+        //     }
+        // }
+        // return q.top();
+    }
+};
