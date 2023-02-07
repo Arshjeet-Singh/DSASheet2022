288f3bcac9e66a6c1b2dbc3a23dfd92d9e049dc7
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2023-02-07 11:35:02 +0530

    Update Minimum Cost Tree From Leaf Values.cpp

--- Minimum Cost Tree From Leaf Values.cpp
+++ Minimum Cost Tree From Leaf Values.cpp
@@ -1,5 +1,80 @@
 class Solution {
 public:
+int solve(vector<int>& arr, map<pair<int,int>,int> &mp,int l,int r)
+{
+    if(l==r)
+    {
+        return 0;
+    }
+    int ans=INT_MAX;
+    for(int i=l;i<r;i++)
+    {
+        ans=min(ans,mp[{l,i}]*mp[{i+1,r}]+ solve(arr,mp,l,i)+ solve(arr,mp,i+1,r));
+    }
+    return ans;
+}
+int solvem(vector<int>& arr, map<pair<int,int>,int> &mp,int l,int r,vector<vector<int>> &dp)
+{
+    if(l==r)
+    {
+        return 0;
+    }
+    int ans=INT_MAX;
+    if(dp[l][r]!=-1)
+    {
+        return dp[l][r];
+    }
+    for(int i=l;i<r;i++)
+    {
+        ans=min(ans,mp[{l,i}]*mp[{i+1,r}]+ solvem(arr,mp,l,i,dp)+ solvem(arr,mp,i+1,r,dp));
+    }
+    return dp[l][r]=ans;
+}
+int solvet(vector<int>& arr,map<pair<int,int>,int> &mp)
+{
+    int n=arr.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int l=n-1;l>=0;l--)
+    {
+        for(int r=0;r<n;r++)
+        {
+            if(l==r)
+            {
+                dp[l][r]=0;
+                continue;
+            }
+            int ans=1e9;
+            for(int i=l;i<r;i++)
+            {
+                ans=min(ans,mp[{l,i}]*mp[{i+1,r}]+ dp[l][i]+ dp[i+1][r]);
+            }
+            dp[l][r]=ans;
+        }
+    }
+    return dp[0][n-1];
+}
+    int mctFromLeafValues(vector<int>& arr) {
+        int n=arr.size();
+        map<pair<int,int>,int> mp;
+        for(int i=0;i<n;i++)
+        {
+             mp[{i,i}]=arr[i];
+            for(int j=i+1;j<n;j++)
+            {
+                int b=mp[{i,j-1}];
+                mp[{i,j}]=max(arr[j],b);
+            }
+        }
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solvem(arr,mp,0,n-1,dp);      
+        return solvet(arr,mp);
+    }
+
+};
+
+//before course
+class Solution {
+public:
     map<pair<int,int>,pair<int,int>> dp;
     pair<int,int> go(vector<int>& arr,int st,int en)
     {
