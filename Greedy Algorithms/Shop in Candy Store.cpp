c5d6d42030581cd150ddb18109d24b1fd7a6a635
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2023-03-08 11:33:24 +0530

    Create Shop in Candy Store.cpp

---
+++ Greedy Algorithms/Shop in Candy Store.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> candyStore(int candies[], int N, int K)
+    {
+        vector<int> v;
+        int i=0,j=N-1;
+        int mini=0;
+        sort(candies,candies+N);
+        while(i<=j && i<N && j>=0)
+        {
+            mini+=candies[i++];
+            j-=K;
+        }
+        int maxi=0;
+        i=0,j=N-1;
+        while(i<=j && i<N && j>=0)
+        {
+            maxi+=candies[j--];
+            i+=K;
+        }
+        v.push_back(mini);
+        v.push_back(maxi);
+        return v;
+        // Write Your Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        int candies[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> candies[i];
+        }
+
+        Solution ob;
+        vector<int> cost = ob.candyStore(candies, N, K);
+        cout << cost[0] << " " << cost[1] << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
